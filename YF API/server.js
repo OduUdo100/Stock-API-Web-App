2a2ee07d7834059a8e1030ca5872974247d6dfd3
OduUdo100 <od.bro29al@gmail.com>
2023-05-01 23:02:37 -0400

    decided to add a header and create new html pages

--- YF API/server.js
+++ YF API/server.js
@@ -9,13 +9,14 @@ http.createServer((req, res) => {
       return res.end('404 Not Found');
     }
 
-    let contentType = 'text/html'; 
+    let contentType = 'text/html';
+    
     if(filename.endsWith('.css')){
         contentType = 'text/css'; 
     } else if (filename.endsWith('.js')) {
        contentType = 'application/javascript'; 
     }
-    //res.writeHead(200, {'Content-Type': 'application/javascript'});
+    res.writeHead(200, {'Content-Type': 'application/javascript'});
     res.write(data);
     return res.end();
   });
